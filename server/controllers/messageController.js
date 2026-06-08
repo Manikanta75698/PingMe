40afe537602f6a98dd5253d7458c0bdf707c1cc7
Manikanta@756 <kasireddymanikantha@gmail.com>
2026-06-08 23:38:51 +0530

    Added username support and real-time chat

---
+++ server/controllers/messageController.js
@@ -0,0 +1,36 @@
+const Message = require("../models/Message");
+
+const sendMessage = async (req, res) => {
+  try {
+    const { username, text } = req.body;
+
+    const message = await Message.create({
+      username,
+      text,
+    });
+
+    res.status(201).json(message);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+const getMessages = async (req, res) => {
+  try {
+    const messages = await Message.find()
+      .sort({ createdAt: 1 });
+
+    res.json(messages);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  sendMessage,
+  getMessages,
+};
--- server/server.js
+++ server/server.js
@@ -1,13 +1,40 @@
 const authRoutes = require("./routes/authRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+
+const http = require("http");
+const { Server } = require("socket.io");
+
 require("dotenv").config();
 
 const app = express();
 
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
 app.use(cors());
 app.use(express.json());
+
+io.on("connection", (socket) => {
+  console.log("User Connected:", socket.id);
+
+  socket.on("send_message", (data) => {
+    io.emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected:", socket.id);
+  });
+});
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -17,10 +44,12 @@ mongoose
 app.get("/", (req, res) => {
   res.send("PingMe API Running 🚀");
 });
+
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
