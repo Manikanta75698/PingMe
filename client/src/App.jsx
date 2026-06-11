916cd14c16598d71d51f4ff9258b707e425ed45d
Manikanta@756 <kasireddymanikantha@gmail.com>
2026-06-11 23:55:41 +0530

    Fix VerifyOTP import in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import VerifyOTP from "./pages/VerifyOTP";
 import Chat from "./pages/Chat";
 import ProtectedRoute from "./components/ProtectedRoute";
 
