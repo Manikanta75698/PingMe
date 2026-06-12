243bc94721268f6e97fe7cd819b2bc6cb16feced
Manikanta@756 <kasireddymanikantha@gmail.com>
2026-06-12 10:33:23 +0530

    Fix profile upload authentication

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,12 +5,54 @@ const upload = require("../config/multer");
 
 const {
   uploadProfilePic,
+  updateProfile,
+  followUser,
+  unfollowUser,
+  getUserProfile,
 } = require("../controllers/userController");
 
+const { protect } = require("../middleware/authMiddleware");
+
+
+// Upload Profile Picture
 router.put(
   "/upload",
+  protect,
   upload.single("profilePic"),
   uploadProfilePic
 );
+
+
+// Update Profile Details
+router.put(
+  "/update",
+  protect,
+  updateProfile
+);
+
+
+// Follow User
+router.put(
+  "/follow/:id",
+  protect,
+  followUser
+);
+
+
+// Unfollow User
+router.put(
+  "/unfollow/:id",
+  protect,
+  unfollowUser
+);
+
+
+// Get User Profile
+router.get(
+  "/:id",
+  protect,
+  getUserProfile
+);
+
 
 module.exports = router;
