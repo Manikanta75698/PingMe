a83c469485c73cb97ce7a762ca26259153e34abe
Manikanta@756 <kasireddymanikantha@gmail.com>
2026-07-09 15:37:05 +0530

    feat: add chat image upload with Cloudinary

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,12 +2,8 @@ import axios from "axios";
 
 const API_URL = "https://pingme-m8y1.onrender.com/api";
 
-
 const api = axios.create({
   baseURL: API_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 // Automatically attach JWT token
--- client/src/services/chatService.js
+++ client/src/services/chatService.js
@@ -4,8 +4,4 @@ export const getConversation = (userId) =>
   api.get(`/messages/conversation/${userId}`);
 
 export const sendMessage = (data) =>
-  api.post("/messages/send", data, {
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-  });
+  api.post("/messages/send", data);
